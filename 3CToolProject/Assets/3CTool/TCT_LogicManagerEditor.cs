ca9690ae2c5b3a56b96aca03905a07754ffacc70
William Morelli <morelli186@gmail.com>
2020-06-14 19:20:58 +0200

    add works

---
+++ 3CToolProject/Assets/3CTool/TCT_LogicManagerEditor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Unitility;
+
+[CustomEditor(typeof(TCT_LogicManager))]
+public class TCT_LogicManagerEditor : EditorCustom<TCT_LogicManager>
+{
+   
+}
