a1ed084d4fe42abfcdc72fa173db4224214bcd1b
William Morelli <morelli186@gmail.com>
2020-08-03 20:23:28 +0100

    Add actionInput

---
+++ 3CToolProject/Assets/3CTool/Scripts/Controller/TCT_ActionInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TCT_ActionInput
+{
+    public static Action ActionInput = null;
+
+
+
+    List<KeyCode> allKeyCodes = new List<KeyCode>();
+
+}
