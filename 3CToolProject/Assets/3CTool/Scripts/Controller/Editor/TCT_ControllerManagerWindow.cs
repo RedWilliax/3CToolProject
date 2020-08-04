41e8a3e0c20febd8277debe7f593cc4f7f16687b
William Morelli <morelli186@gmail.com>
2020-08-04 12:46:14 +0100

    begin ControllerManager + add AxisInput

--- 3CToolProject/Assets/3CTool/Scripts/Controller/Editor/TCT_ControllerManagerWindow.cs
+++ 3CToolProject/Assets/3CTool/Scripts/Controller/Editor/TCT_ControllerManagerWindow.cs
@@ -3,7 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+using Unitility;
+
 public class TCT_ControllerManagerWindow : EditorWindow
 {
-   
+
+    [SerializeField] List<TCT_ActionInput> AllActionInput = new List<TCT_ActionInput>();
+
+    [SerializeField] List<TCT_AxisInput> AllAxisInput = new List<TCT_AxisInput>();
+
+    bool showActionInput = false;
+
+
+    private void OnGUI()
+    {
+
+        //GUILayout.BeginArea(new Rect(10, 10, position.width * 0.15f, position.height));
+
+        GUILayout.BeginHorizontal();
+
+        EditorLayout.Button("Add ActionInput", CreateNewActionInput);
+
+        EditorLayout.Button("Add AxisInput", CreateNewAxisInput);
+
+        GUILayout.EndHorizontal();
+
+        EditorLayout.Space();
+
+        showActionInput = EditorGUILayout.Foldout(showActionInput, "Actions Input");
+
+        DrawAllActionInput(showActionInput);
+
+
+
+
+        //GUILayout.EndArea();
+
+    }
+
+    void DrawAllActionInput(bool _show)
+    {
+        if (!_show) return;
+
+        for (int i = 0; i < AllActionInput.Count; i++)
+        {
+            bool _showInputs = true;
+
+            GUILayout.BeginHorizontal();
+
+            _showInputs = EditorGUILayout.Foldout(_showInputs, "");
+
+            EditorGUILayout.TextField(AllActionInput[i].name, GUILayout.MaxWidth(200));
+
+            EditorLayout.Button("+", AllActionInput[i].AddKey, KeyCode.A);
+
+            //DrawKeysActionInput(_showInputs, AllActionInput[i]);
+
+            GUILayout.EndHorizontal();
+
+            EditorLayout.Space();
+        }
+    }
+
+    void DrawKeysActionInput(bool _show, TCT_ActionInput _action)
+    {
+
+        if (_action == null || _action.AllKeyCodes.Count < 1) return;
+
+        for (int i = 0; i < _action.AllKeyCodes.Count; i++)
+        {
+            EditorGUILayout.TextField(_action.AllKeyCodes[i].ToString());
+
+            EditorLayout.Space();
+        }
+
+    }
+
+    void CreateNewActionInput()
+    {
+        AllActionInput.Add(new TCT_ActionInput());
+    }
+
+    void CreateNewAxisInput()
+    {
+
+
+    }
+
 }
--- 3CToolProject/Assets/3CTool/Scripts/Controller/TCT_ActionInput.cs
+++ 3CToolProject/Assets/3CTool/Scripts/Controller/TCT_ActionInput.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class TCT_ActionInput
 {
-    public static Action ActionInput = null;
+    public static Action<bool> ActionInput = null;
 
-
+    public string name = "Default_ActionName";
 
     List<KeyCode> allKeyCodes = new List<KeyCode>();
+
+    public string Name { get; set; }
+
+    public List<KeyCode> AllKeyCodes { get; set; }
+
+    public void AddKey(KeyCode _key)
+    {
+        ManageKey(true, _key);
+    }
+
+    public void RemoveKey(KeyCode _key)
+    {
+        ManageKey(false, _key);
+    }
+
+    void ManageKey(bool _add, KeyCode _key)
+    {
+        if (_add ? ExistKeyCode(_key) : !ExistKeyCode(_key)) return;
+
+        if (_add) allKeyCodes.Add(_key);
+
+        else allKeyCodes.Remove(_key);
+    }
+
+
+    public bool ExistKeyCode(KeyCode _key)
+    {
+        return allKeyCodes.Any(n => n == _key);
+    }
+
 
 }
